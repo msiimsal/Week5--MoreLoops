5c3cd4a973653a2dd1ef92a6e7c53f7b5d7753da
Merlin Siimsalu <msiimsal@tktk.ee>
2021-12-03 19:53:35 +0200

    add new projects

---
+++ OneToTen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OneToTen
+{
+    class Program
+    {
+        private static int userNumber;
+
+        static void Main(string[] args)
+        {
+            // programm küsib kasutajal sisestada kolm numbrit
+            //programm kuvab nende numbrite summat
+
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Sisesta number:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                sum = sum + userNumber;
+
+            }
+
+            Console.WriteLine($"Your total is: {sum}");
+
+        }
+    }
+}
